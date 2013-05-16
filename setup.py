c1d067947f4e8c97c5055bd8f5e30026313e1dba
Roberto Riggio <roberto.riggio@create-net.org>
2013-05-16 14:41:41 +0200

    refactor modules into one energino package

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ setup(name="pachubino",
 	author="Roberto Riggio",
 	author_email="roberto.riggio@create-net.org",
 	url="https://github.com/rriggio/energino",
-	py_modules=['energino','pachubino','dispatcher'],
 	long_description="Energino distributed energy monitoring toolkit",
 	data_files = [('etc/', ['pachubino.conf'])],
-	entry_points={"console_scripts": ["energino=energino:main", "pachubino=pachubino:main"]},
+	entry_points={"console_scripts": ["energino=energino.energino:main", "pachubino=energino.pachubino:main"]},
+	packages=['energino'],
 	license = "Python",
 	platforms="any"
 )
