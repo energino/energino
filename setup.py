8327cf525da25465e194e658d5fafc086c57f05f
Roberto Riggio <roberto.riggio@create-net.org>
2013-07-29 12:39:25 +0200

    More Xively strings

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ from distutils.core import setup
 if sys.version < '2.7':
 	raise ValueError("Sorry Python versions older than 2.7 are not supported")
 
-setup(name="pachubino",
+setup(name="energino",
 	version="0.1",
 	description="PyEnergino",
 	author="Roberto Riggio",
 	author_email="roberto.riggio@create-net.org",
 	url="https://github.com/rriggio/energino",
 	long_description="Energino distributed energy monitoring toolkit",
-	data_files = [('etc/', ['pachubino.conf'])],
-	entry_points={"console_scripts": ["energino=energino.energino:main", "pachubino=energino.pachubino:main"]},
+	data_files = [('etc/', ['xively.conf'])],
+	entry_points={"console_scripts": ["energino=energino.energino:main", "xively_client=energino.xively_client:main"]},
 	packages=['energino'],
 	license = "Python",
 	platforms="any"
