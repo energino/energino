8b4d1f76a2c3fa8d8020a3f6e11a3315483f1d8a
Roberto Riggio <roberto.riggio@create-net.org>
2014-08-25 12:21:31 +0200

    use int instead of double

--- sketches/libraries/SMA/sma.h
+++ sketches/libraries/SMA/sma.h
@@ -9,82 +9,80 @@
 #include <assert.h>
 
 class SMA {
+public:
+  SMA(unsigned int period) :
+    period(period), window(new int[period]), head(NULL), tail(NULL),
+        total(0) {
+    assert(period >= 1);
+  }
+  ~SMA() {
+    delete[] window;
+  }
 
-  public:
-
-    SMA(unsigned int period) :
-      period(period), window(new long[period]), head(NULL), tail(NULL), total(0) {
-      assert(period >= 1);
-    }
-
-    ~SMA() {
-      delete[] window;
-    }
-
-    // Adds a value to the average, pushing one out if nescessary
-    void add(long val) {
-      // Special case: Initialization
-      if (head == NULL) {
-        head = window;
-        *head = val;
-        tail = head;
-        inc(tail);
-        total = val;
-        return;
-      }
-
-      // Were we already full?
-      if (head == tail) {
-        // Fix total-cache
-        total -= *head;
-        // Make room
-        inc(head);
-      }
-
-      // Write the value in the next spot.
-      *tail = val;
+  // Adds a value to the average, pushing one out if nescessary
+  void add(int val) {
+    // Special case: Initialization
+    if (head == NULL) {
+      head = window;
+      *head = val;
+      tail = head;
       inc(tail);
-
-      // Update our total-cache
-      total += val;
+      total = val;
+      return;
     }
 
-    // Returns the average of the last P elements added to this SMA.
-    // If no elements have been added yet, returns 0.0
-    double avg() const {
-      ptrdiff_t size = this->size();
-      if (size == 0) {
-        return 0; // No entries => 0 average
-      }
-      return total / (double) size; // Cast to double for floating point arithmetic
+    // Were we already full?
+    if (head == tail) {
+      // Fix total-cache
+      total -= *head;
+      // Make room
+      inc(head);
     }
 
-  private:
-    unsigned int period;
-    long * window; // Holds the values to calculate the average of.
+    // Write the value in the next spot.
+    *tail = val;
+    inc(tail);
 
-    // Logically, head is before tail
-    long * head; // Points at the oldest element we've stored.
-    long * tail; // Points at the newest element we've stored.
+    // Update our total-cache
+    total += val;
+  }
 
-    long total; // Cache the total so we don't sum everything each time.
-
-    // Bumps the given pointer up by one.
-    // Wraps to the start of the array if needed.
-    void inc(long * & p) {
-      if (++p >= window + period) {
-        p = window;
-      }
+  // Returns the average of the last P elements added to this SMA.
+  // If no elements have been added yet, returns 0.0
+  double avg() const {
+    ptrdiff_t size = this->size();
+    if (size == 0) {
+      return 0; // No entries => 0 average
     }
+    return total / (double) size; // Cast to double for floating point arithmetic
+  }
 
-    // Returns how many numbers we have stored.
-    ptrdiff_t size() const {
-      if (head == NULL)
-        return 0;
-      if (head == tail)
-        return period;
-      return (period + tail - head) % period;
+private:
+  unsigned int period;
+  int * window; // Holds the values to calculate the average of.
+
+  // Logically, head is before tail
+  int * head; // Points at the oldest element we've stored.
+  int * tail; // Points at the newest element we've stored.
+
+  int total; // Cache the total so we don't sum everything each time.
+
+  // Bumps the given pointer up by one.
+  // Wraps to the start of the array if needed.
+  void inc(int * & p) {
+    if (++p >= window + period) {
+      p = window;
     }
+  }
+
+  // Returns how many numbers we have stored.
+  ptrdiff_t size() const {
+    if (head == NULL)
+      return 0;
+    if (head == tail)
+      return period;
+    return (period + tail - head) % period;
+  }
 };
 
 #endif
